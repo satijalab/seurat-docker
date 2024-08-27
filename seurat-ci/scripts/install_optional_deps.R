300d0a1f9e5cc3e0f4415ea3428af69194156e9a
David Collins <dcollins@nygenome.org>
2024-08-27 16:44:27 -0400

    Use remotes to install dependencies

--- seurat-ci/scripts/install_optional_deps.R
+++ seurat-ci/scripts/install_optional_deps.R
@@ -1,19 +1,7 @@
 #!/usr/bin/env Rscript
 
-is_installed <- function(pkg) {
-    return (requireNamespace(pkg, quietly = TRUE))
+if (!requireNamespace("remotes", quietly = TRUE)) {
+    install.packages("remotes")
 }
 
-if (!is_installed("desc")) {
-    install.packages("desc")
-}
-
-all_deps <- desc::desc_get_deps()
-
-optional_types <- c("Suggests", "Enhances")
-optional_deps <- all_deps[all_deps$type %in% optional_types, ]
-optional_pkgs <- optional_deps[optional_deps$package != "R", ]$package
-
-pkgs_to_install <- optional_pkgs[!sapply(optional_pkgs, is_installed)]
-
-install.packages(pkgs_to_install)
+remotes::install_deps(dependencies = c("Suggests", "Enhances"))
--- seurat-ci/scripts/install_required_deps.R
+++ seurat-ci/scripts/install_required_deps.R
@@ -1,19 +1,7 @@
 #!/usr/bin/env Rscript
 
-is_installed <- function(pkg) {
-    return (requireNamespace(pkg, quietly = TRUE))
+if (!requireNamespace("remotes", quietly = TRUE)) {
+    install.packages("remotes")
 }
 
-if (!is_installed("desc")) {
-    install.packages("desc")
-}
-
-all_deps <- desc::desc_get_deps()
-
-optional_types <- c("Suggests", "Enhances")
-required_deps <- all_deps[!all_deps$type %in% optional_types, ]
-required_pkgs <- required_deps[required_deps$package != "R", ]$package
-
-pkgs_to_install <- required_pkgs[!sapply(required_pkgs, is_installed)]
-
-install.packages(pkgs_to_install)
+remotes::install_deps()

965a785969953c0980f0c0211b678ed28a5d2a9f
David Collins <dcollins@nygenome.org>
2024-08-27 00:32:58 -0400

    Fixup install_required_deps.R

--- seurat-ci/scripts/install_required_deps.R
+++ seurat-ci/scripts/install_required_deps.R
@@ -14,6 +14,6 @@ optional_types <- c("Suggests", "Enhances")
 required_deps <- all_deps[!all_deps$type %in% optional_types, ]
 required_pkgs <- required_deps[required_deps$package != "R", ]$package
 
-pkgs_to_install <- required_pkgs[sapply(required_pkgs, !is_installed)]
+pkgs_to_install <- required_pkgs[!sapply(required_pkgs, is_installed)]
 
 install.packages(pkgs_to_install)

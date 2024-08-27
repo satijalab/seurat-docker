a35e8ec92d9df9b6c5ed4f0ac6a3d004b7b0197e
David Collins <dcollins@nygenome.org>
2024-08-27 00:08:36 -0400

    Add run_check.R for seurat-ci

---
+++ seurat-ci/scripts/run_checks.R
@@ -0,0 +1,12 @@
+#!/usr/bin/env Rscript
+
+if (!requireNamespace("rcmdcheck", quietly = TRUE)) {
+    install.packages("rcmdcheck")
+}
+
+rcmdcheck::rcmdcheck(
+    args = c("--no-manual", "--as-cran"), 
+    build_args = "--no-manual", 
+    error_on = "warning", 
+    check_dir = "check"
+)

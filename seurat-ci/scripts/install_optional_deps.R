fed58213d32b9211fdbdec962ba054ee84efc0dd
David Collins <dcollins@nygenome.org>
2024-08-27 00:08:14 -0400

    Add install_optional_deps.R for seurat-ci

---
+++ seurat-ci/scripts/install_optional_deps.R
@@ -0,0 +1,19 @@
+#!/usr/bin/env Rscript
+
+is_installed <- function(pkg) {
+    return (requireNamespace(pkg, quietly = TRUE))
+}
+
+if (!is_installed("desc")) {
+    install.packages("desc")
+}
+
+all_deps <- desc::desc_get_deps()
+
+optional_types <- c("Suggests", "Enhances")
+optional_deps <- all_deps[all_deps$type %in% optional_types, ]
+optional_pkgs <- required_deps[required_deps$package != "R", ]$package
+
+pkgs_to_install <- optional_pkgs[sapply(optional_pkgs, !is_installed)]
+
+install.packages(pkgs_to_install)

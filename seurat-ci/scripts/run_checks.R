3d5f1c9bd771a706ce6f1a676b4cedc6c8191d2d
David Collins <dcollins@nygenome.org>
2024-09-04 13:12:52 -0400

    Add devtools::clean_dll to run_checks.R

--- seurat-ci/scripts/run_checks.R
+++ seurat-ci/scripts/run_checks.R
@@ -4,6 +4,7 @@ if (!requireNamespace("rcmdcheck", quietly = TRUE)) {
     install.packages("rcmdcheck")
 }
 
+devtools::clean_dll()
 rcmdcheck::rcmdcheck(
     args = c("--as-cran"), 
     error_on = "warning"
